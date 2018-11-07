d49f86d8072ae5ea25a23a9d71d7733ddd3d7785
Sullivan Tanaka <30957305+sullivantanaka@users.noreply.github.com>
2018-11-07 11:53:04 -0200

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,20 @@
 require "appium_capybara"
 
-desired_caps_android = {
+desired_caps_android_chrome = {
   platform:        "android",
   deviceName:      "simulator",
   platformName:    "Android",
-  platformVersion: "6.0",
-  #app:             "full/path/to/app.zip"
+  #platformVersion: "6.0", # usar somente se souber a versao exata do sistema
   browserName: "Chrome"
+}
+
+desired_caps_android_app = {
+  platform:        "android",
+  deviceName:      "simulator",
+  platformName:    "Android",
+  #platformVersion: "6.0", # usar somente se souber a versao exata do sistema
+  appActivity:     "com.android.calculator2.Calculator", # obter informacao usando APK Info ou outro app
+  appPackage:      "com.android.calculator2"
 }
 
 url = "http://localhost:4723/wd/hub" # or a sauce labs url
@@ -17,7 +25,7 @@ Capybara.register_driver(:appium) do |app|
     }
     all_options = {
       appium_lib:  appium_lib_options,
-      caps:        desired_caps_android
+      caps:        desired_caps_android_chrome #definir qual tipo de dispositivo e se é app ou navegador a ser testado
     }
     Appium::Capybara::Driver.new app, all_options
 end
